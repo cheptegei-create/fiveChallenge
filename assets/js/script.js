fdd7dcc240b31f3cbfa523aea85e681cb08e650a
[David Cheptegei] <[davidchepte@gmail.com]>
2022-12-12 01:00:50 -0500

    modified the js file and html file

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  //setting up and displaying the current date and live time on the header
   window.onload = displayClock();
   function displayClock() {
     var currentDay = dayjs().format("MMMM D, YYYY h:mm:ss:a");
@@ -9,6 +10,60 @@ $(function () {
 
     setTimeout(displayClock, 1000);
   }
+
+  //Adding a click event listener on the save button
+  $(".saveBtn").on("click", function () {
+    //get the time value and event desription
+    var description = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+    // var plans = {
+    //   agenda: description,
+    //   currentTime: time,
+    // };
+
+    localStorage.setItem(time, description);
+    alert("plans stored in local storage");
+  });
+
+  function timeKeeper() {
+    //setting current hour
+    var moment = dayjs().format("h");
+
+    //setting a loop on the blocks
+    $(".time-block").each(function () {
+      var catchTime = parseInt($(this).attr("id").split("hour")[1]);
+
+      //introducing an if condition to color code the blocks
+      if (catchTime < moment) {
+        $(this).removeClass('future');
+        $(this).removeClass('present');
+        $(this).addClass("past");
+      } else if (catchTime === moment) {
+        $(this).removeClass('future');
+        $(this).removeClass('past');
+        $(this).addClass("present");
+      } else {
+        $(this).removeClass('past');
+        $(this).removeClass('present');
+        $(this).addClass("future");
+      }
+    });
+  }
+
+  //getting items from local storage
+
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+
+  timeKeeper();
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
